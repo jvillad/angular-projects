23a18b895ff4ea1a5b801d88719b4637210c7052
jvillad <jyvillad@gmail.com>
2023-04-17 23:33:08 +1000

    product interface

---
+++ intro-angular/src/app/common/models/product.interface.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+  productId: number;
+  productName: string;
+  productCode: string;
+  productBrewDate: string;
+  productDescription: string;
+  productPrice: number;
+  productRating: number;
+  productImageUrl: string;
+}
