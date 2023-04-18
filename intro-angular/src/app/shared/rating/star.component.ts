a62c30c1bd7a6e80c6b17e3979b0603ff9972e8d
jvillad <jyvillad@gmail.com>
2023-04-18 14:28:24 +1000

    accept description for popup to display

--- intro-angular/src/app/shared/rating/star.component.ts
+++ intro-angular/src/app/shared/rating/star.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'pm-star',
@@ -7,10 +13,22 @@ import { Component, Input, OnChanges } from '@angular/core';
 })
 export class StarComponent implements OnChanges {
   @Input() rating: number = 0;
+  @Input() description: string = '';
   cropWidth: number = 75;
+  popup: Boolean = false;
+  // @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
 
   // determines the width of stars to display
   ngOnChanges(): void {
     this.cropWidth = (this.rating * 80) / 5;
   }
+
+  // on click event handler
+  onClick(): void {
+    this.popup = true;
+  }
+
+  // onrRatingClicked(message: string): void {
+  //   this.ratingClicked.emit(`Item has ${this.rating}`);
+  // }
 }
