294d293816dac0d718dde50f0b2f7bb7abfbb96e
johnrey.villademosa <jyvillad@gmail.com>
2023-07-28 23:52:58 +1000

    seed

--- projects/run-tribe/src/app/app.component.ts
+++ projects/run-tribe/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Runtribe } from './runtribe';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'homes';
+  title = 'runtribe';
+
+  runTribeList: Runtribe[] = [
+    {
+      name: 'ACME Run Tribe',
+      city: 'Sydney',
+      state: 'NSW',
+      postcode: '2065',
+      imageUrl:
+        'https://img.freepik.com/free-vector/cute-people-running_138676-3302.jpg?w=740&t=st=1690517278~exp=1690517878~hmac=ffe58399f2823ab62c8885b007c18345e9350c93003f887ba7cf9dce61d13a59',
+      paidMembership: false,
+    },
+    {
+      name: 'Worst Run Tribe',
+      city: 'Sydney',
+      state: 'NSW',
+      postcode: '2065',
+      imageUrl:
+        'https://img.freepik.com/free-vector/cute-people-running_138676-3302.jpg?w=740&t=st=1690517278~exp=1690517878~hmac=ffe58399f2823ab62c8885b007c18345e9350c93003f887ba7cf9dce61d13a59',
+      paidMembership: true,
+    },
+  ];
+
+  selectedRunTribe: Runtribe | undefined;
+
+  updateSelectedRunGroup(location: Runtribe) {
+    this.selectedRunTribe = location;
+  }
 }
