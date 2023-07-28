f400068d2b7240bb38fa19ae6256a5f4e576c55c
johnrey.villademosa <jyvillad@gmail.com>
2023-07-28 23:53:20 +1000

    running group component

--- projects/run-tribe/src/app/running-list/running-list.component.ts
+++ projects/run-tribe/src/app/running-list/running-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Runtribe } from '../runtribe';
 @Component({
   selector: 'app-running-list',
   templateUrl: './running-list.component.html',
-  styleUrls: ['./running-list.component.css']
+  styleUrls: ['./running-list.component.css'],
 })
 export class RunningListComponent {
+  @Input() tribeList: Runtribe[] = [];
+  results: Runtribe[] = [];
+  @Output() selectedRunTribe = new EventEmitter<Runtribe>();
+  searchRunGroups(searchText: string) {
+    if (!searchText) return;
+    this.results = this.tribeList.filter((location) =>
+      location.city.toLowerCase().includes(searchText.toLowerCase())
+    );
+  }
 
+  selectRunTribe(location: Runtribe) {
+    this.selectedRunTribe.emit(location);
+  }
 }
